ca9308a2dbbac7b881a84d6d22dff0d8a515a8a3
manoj kumar maury <manojkumarmaury563@gmail.com>
2024-05-24 11:42:13 +0530

    added model Subscribe, Review, Faq and Contact

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Postings,Message,Chatbox,Appliedfor
+
+from .models import Postings, Message, Chatbox, Appliedfor, Subscribe, Review, Faq, Contact
+
+
+
 
 # Register your models here.
 admin.site.register(Postings)
 admin.site.register(Message)
 admin.site.register(Chatbox)
 admin.site.register(Appliedfor)
+
+admin.site.register(Subscribe)
+admin.site.register(Review)
+admin.site.register(Faq)
+admin.site.register(Contact)
+
+
+
+
+
