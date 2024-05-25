17df5f09f8c6653e8c48591732172016ff8cbf1f
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-05-25 10:57:57 +0530

    added url path in main project

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 urlpatterns = [ 
     path('',include("app.urls")),
     path('',include("user_account.urls")),
+    path('',include("blog.urls")),
     # path('admin/',include('customadmin.urls')),
     path('admin/', admin.site.urls),
 ]
