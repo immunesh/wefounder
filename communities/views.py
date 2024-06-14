59abd8c326afcf670e82b532977e037e28bf5f54
Neha Singh <nehasingh973199@gmail.com>
2024-06-14 17:04:28 +0530

    Made changes in communiti page

--- communities/views.py
+++ communities/views.py
@@ -1,6 +1,6 @@
 from django.views import View
 from .models import CommunityCategory, CommunityPost
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
@@ -8,7 +8,11 @@ from django.utils.decorators import method_decorator
 @login_required(login_url='signin')
 def Community(request):
     communities = CommunityCategory.objects.all()
-    posts = CommunityPost.objects.all()
+    if request.method == 'POST':
+        data = request.POST.get('search_filter')
+        posts = CommunityPost.objects.filter(title =data)
+    else:
+        posts = CommunityPost.objects.all()
 
     context = {
         'communities': communities,
@@ -16,6 +20,17 @@ def Community(request):
     }
 
     return render(request, 'community.html', context)
+
+@login_required(login_url='signin')
+def CommunityFilter(request,str):
+    posts = CommunityPost.objects.get(category=str)
+    context = {
+        'posts': posts
+    }
+    return redirect('/communityfilter/')
+
+
+    
 
 @method_decorator(login_required(login_url='signin'), name='dispatch')
 class CommunitySingle(View):
